4d6999b97668e4cdf221958cd3e82583d91c3080
Marko1310 <marko.cabo13@gmail.com>
2023-07-02 11:32:31 +0200

    stylizing grid elements, adding images and description

--- app/components/ProductsList.tsx
+++ app/components/ProductsList.tsx
@@ -1,4 +1,5 @@
 import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
+import Image from "next/image";
 import React from "react";
 
 type ProductsComponentProps = {
@@ -13,10 +14,30 @@ const ProductsComponent: React.FC<ProductsComponentProps> =
           {products.map((el) => {
             return (
               <div
-                className="w-min-[16rem] w-max-[17.22rem] border-rad h-[19.75rem] rounded-sm border-solid border-red-300 bg-[#F7F7FA] p-2 "
+                className="w-min-[16rem] w-max-[17.22rem] h-max-[19.75rem] flex flex-col justify-center rounded-sm bg-[#F7F7FA]"
                 key={el.id}
               >
-                {el.title}
+                <div className=" relative flex h-full flex-1 justify-center ">
+                  <Image
+                    src={el.thumbnail || ""}
+                    alt="thumbnail"
+                    // width={100}
+                    // height={100}
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    className="h-full w-full object-contain"
+                    // className="h-full w-full object-contain"
+                  />
+                </div>
+
+                <div className="flex h-10 flex-row items-center justify-between px-2">
+                  <p>{el.title?.slice(6)}</p>
+                  <p>
+                    {el.variants[0].prices[0].amount}{" "}
+                    {el.variants[0].prices[0].currency_code}
+                  </p>
+                </div>
               </div>
             );
           })}
